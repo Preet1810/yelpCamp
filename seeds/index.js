253b907cea8a645735f9d15a4399f250f68d89a4
Preet1810 <preetpalsingh1239@gmail.com>
2022-09-17 00:01:40 +0530

    moreErrors

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,13 @@ const { places, descriptors }=require('./seedHelpers');
 const Campground=require('../models/campground');
 const indianCities=require('./indiCites');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+if (process.env.NODE_ENV!=='production') {
+    require('dotenv').config();
+}
+
+const dbUrl=process.env.DB_URL||'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
