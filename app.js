e07ba204b5cc9f14b2bd26cd92da66af279cc66b
Preet1810 <preetpalsingh1239@gmail.com>
2022-09-16 19:57:13 +0530

    app.js

--- app.js
+++ app.js
@@ -10,16 +10,13 @@ const mongoose=require('mongoose');
 const ejsMate=require('ejs-mate');
 const session=require('express-session');
 const flash=require('connect-flash');
+const ExpressError=require('./utils/ExpressError');
+const methodOverride=require('method-override');
 const passport=require('passport');
 const LocalStrategy=require('passport-local');
 const User=require('./models/user');
 const helmet=require('helmet'); //comes with a lot of security headers 
-
 const mongoSanitize=require('express-mongo-sanitize');  //resolves security issue related to mongodb
-
-const ExpressError=require('./utils/ExpressError');
-const methodOverride=require('method-override');
-
 const userRoutes=require('./routes/users');
 const campgroundRoutes=require('./routes/campground');
 const reviewRoutes=require('./routes/reviews');
@@ -42,12 +39,14 @@ db.once("open", () => {
 
 const app=express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.engine('ejs', ejsMate);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // To remove data using these defaults:
 app.use(mongoSanitize());
@@ -133,7 +132,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-app.use(express.static(path.join(__dirname, 'public')))
+
+
 
 
 
